15112ceba5b6fe241e0ff71b3c4bbdccece784c2
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-15 21:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var hitRight = 0;
 var hitLeft = 1;
 
 function moveRight () {
-    if (img.style.marginLeft < 800 & hitRight === 0 & hitLeft == 1){
+    if (img.style.marginLeft < 800 && hitRight === 0 && hitLeft == 1){
         marginLeft = marginLeft + 10;
         img.style.marginLeft = marginLeft + 'px';
     }
@@ -15,7 +15,7 @@ function moveRight () {
         hitRight = 1;
         hitLeft = 0;
     }
-    if (img.style.marginLeft > 50 & hitRight == 1 & hitLeft === 0) {
+    if (img.style.marginLeft > 50 && hitRight == 1 && hitLeft === 0) {
         marginLeft = marginLeft - 10;
         img.style.marginLeft = marginLeft + 'px';
     }
