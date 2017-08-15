ebbc3e3ba824ff209f47150d729166506150ac17
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-15 23:21:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,7 +56,7 @@ submit.onclick = function () {
         }
     };
     
-    var nameInput = document.getElementById('');
+    var nameInput = document.getElementById('name');
     var name = nameInput.value;
     request.open('GET', 'http://nishanthnitw14.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
