b66555fce55fa0947024ba96debdf7ea382455f7
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-15 20:52:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+var img = document.getElementbyId("madi");
+
+var marginLeft = 0;
+
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + '.px';
+}
+
+img.onClick() = function () {
+        var interval = setInterval(moveRight,100);
+}
