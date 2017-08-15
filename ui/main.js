d9acebbf6a0f62d7d394aca337e12261953521e9
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-15 23:13:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,3 +35,29 @@ img.onclick = function () {
     console.log('Moving the Image');
     var interval = setInterval(moveRight, 50);
 };
+
+var submit = document.getElementById('name');
+
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++) {
+                    list = "li" + names[i] + "/li";
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    var nameInput = document.getElementById('');
+    var name = nameInput.value;
+    request.open('GET', 'http://nishanthnitw14.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
