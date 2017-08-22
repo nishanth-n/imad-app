150cb107b47b85600e7ecc66a9ecb3790738c104
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-22 08:33:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+var config = {
+    user: 'nishanth.nitw14',
+    database: 'nishanth.nitw14',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
+
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -95,6 +105,22 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req, res) {
+    // make a select request
+    // return 0 reponse with the results
+    
+    pool.query('SELECT * from test', function(err, result){
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 var names = [];
