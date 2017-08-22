da7ded3c7f5b92796ecf27482e26e058cfbb4afb
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-22 08:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var Pool = require('pg').Pool;
 var config = {
-    user: 'nishanth.nitw14',
-    database: 'nishanth.nitw14',
+    user: 'nishanthnitw14',
+    database: 'nishanthnitw14',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: 'db-nishanthnitw14-17509'
