952a84f1f23289e452efac1fdb0a106d1239d4a0
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-24 09:25:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,8 +136,8 @@ app.get('/submit-name', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     
-    article_to_fetch = req.params.articleName;
-    console.log("Article to Fetch " + article_to_fetch);
+//    article_to_fetch = req.params.articleName;
+//    console.log("Article to Fetch " + article_to_fetch);
     
     pool.query("SELECT * FROM article", function(err, res){
         if(err) {
