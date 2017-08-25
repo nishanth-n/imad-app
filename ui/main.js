673d8161cb134d4903b5e161934feaf0ab5d815d
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-25 17:35:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@ login_submit.onclick = function () {
 
                 var articles = request.responseText;
                 alert('Articles are ' + articles);
-                alert('Articles are ' + articles[0];
+                alert('Articles are ' + articles[0]);
                 articles = JSON.parse(articles);
                 alert('Articles are ' + articles[0]);
                 var list = '';
