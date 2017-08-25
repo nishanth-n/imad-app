fdf59a2f95dc122f07d6847a2d197e58cdade8e7
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-25 14:49:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,20 +136,20 @@ app.get('/submit-name', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
     
-//    article_to_fetch = req.params.articleName;
-//    console.log("Article to Fetch " + article_to_fetch);
+    article_to_fetch = req.params.articleName;
+    console.log("Article to Fetch " + article_to_fetch);
     
-//    pool.query("SELECT * FROM article", function(err, res){
-//        if(err) {
-//            res.status(500).send(err.toString());
-//        } else {
-//            var articleData = result.rows[0];
-//            console.log(articleData);
-//            res.send(createTemplate(articleData));
- //       }
-//    });
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article", function(err, res){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            var articleData = result.rows[0];
+            console.log(articleData);
+            res.send(createTemplate(articleData));
+       }
+    });
+//  var articleName = req.params.articleName;
+//  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
