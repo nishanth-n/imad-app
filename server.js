8ba2a95a6e3064416bc8b092af7291e68a6a75b3
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-25 20:51:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,19 +92,20 @@ app.get('/submit-name', function (req, res) {
 
 
 app.get('/articles/:articleName', function (req, res) {
-    
-    pool.query("SELECT * FROM articles WHERE title = '" + req.params.articleName + "'", function(err, result){
-        if(err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
+    if(req.session && req.session.auth && req.session.auth.userId) {
+        pool.query("SELECT * FROM articles WHERE title = '" + req.params.articleName + "'", function(err, result){
+            if(err) {
+                res.status(500).send(err.toString());
             } else {
-                var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-       }
-    });
+                if (result.rows.length === 0) {
+                    res.status(404).send('Article not found');
+                } else {
+                    var articleData = result.rows[0];
+                    res.send(createTemplate(articleData));
+                }
+           }
+        });
+    }
 });
 
 app.post('/login', function (req, res) {
