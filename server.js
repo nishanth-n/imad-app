5b1a0ddf61d104aff7058c8b8e47372a46866edb
nishanth-n <nishanth.nitw14@gmail.com>
2017-08-25 16:37:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 var config = {
     user: 'nishanthnitw14',
     database: 'nishanthnitw14',
@@ -14,6 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     var title = data.title;
